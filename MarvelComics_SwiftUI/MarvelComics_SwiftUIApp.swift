ca2a2f67b77632641990537cfc8a758be54fd9ab
mahesh_lad <mlad.mahesh.lad@gmail.com>
2023-06-05 21:50:49 +0100

    Initial Commit

---
+++ MarvelComics_SwiftUI/MarvelComics_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelComics_SwiftUIApp.swift
+//  MarvelComics_SwiftUI
+//
+//  Created by mahesh lad on 05/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MarvelComics_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
